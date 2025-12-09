17452fd753eac4f9e1fa41204bc9e414a5d2da19
Ayush Verma <tgmcodeyogi@gmail.com>
2025-12-09 23:22:41 +0530

    edit

--- src/app.js
+++ src/app.js
@@ -14,14 +14,17 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://sparkling-khapse-134e85.netlify.app/",
+      "https://sparkling-khapse-134e85.netlify.app",
       "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["content-type", "authorization"],
   })
 );
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
