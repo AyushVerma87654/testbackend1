b88e55d91362cd84d168338623374469f3e30964
Ayush Verma <tgmcodeyogi@gmail.com>
2025-12-09 23:16:57 +0530

    edit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,17 @@ import * as EnvironmentController from "./controllers/EnvironmentController.js";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://sparkling-khapse-134e85.netlify.app/",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
